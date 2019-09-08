9ee59a9cf639234b7ec4037a27b1d2ef5b574c2b
Aaron Proctor <github@aaronproctor.uk>
2019-09-08 15:32:15 +0100

    Refactor into functions; fixed some formatting and sorting issues; removed unplaced players from updates

--- index.js
+++ index.js
@@ -84,6 +84,15 @@ bot.on('message', message => {
                     console.error(e);
                     message.channel.send("Oops, something when catastrophically wrong. It's probably Chufty's fault...");
                 });
+                break;
+            case 'update':
+                if (message.author.tag == 'Chufty#6569') {
+                    adhocUpdate([message.guild]);
+                }
+                else {
+                    message.channel.send('Only special people may invoke this command');
+                }
+                break;
         }
     }
 });
@@ -107,11 +116,171 @@ const getGainRole = function(stats, gain) {
 
 const getProfile = promisify(overwatch.getProfile);
 
-// TODO: Split into functions to unwrap brain
+function getUpdate(users) {
+    return Promise.all(
+        users.map(async user => {
+          const player = await getProfile('pc', 'eu', user.Battletag.S.replace('#', '-'));
+            user.stats = {
+                tank: {
+                    rank: player.competitive.tank.rank,
+                    gain: player.competitive.tank.rank - (user.TankSR.N || player.competitive.tank.rank)
+                },
+                damage: {
+                    rank: player.competitive.damage.rank,
+                    gain: player.competitive.damage.rank - (user.DamageSR.N || player.competitive.damage.rank)
+                },
+                support: {
+                    rank: player.competitive.support.rank,
+                    gain: player.competitive.support.rank - (user.SupportSR.N || player.competitive.support.rank)
+                },
+                best: Math.max(player.competitive.support.rank || 0, player.competitive.damage.rank || 0, player.competitive.tank.rank || 0)
+            };
+            user.displayName = user.Battletag.S.split('#')[0];
+            user.gains = {
+                best: {
+                    gain: Math.max(user.stats.tank.gain || 0, user.stats.damage.gain || 0, user.stats.support.gain || 0)
+                },
+                worst: {
+                    gain: Math.min(user.stats.tank.gain || 0, user.stats.damage.gain || 0, user.stats.support.gain || 0)
+                }
+            };
+            user.gains.best.role = getGainRole(user.stats, user.gains.best.gain);
+            user.gains.worst.role = getGainRole(user.stats, user.gains.worst.gain);
+            return user;
+        }));
+}
 
-schedule.scheduleJob({hour: 9, minute: 30}, () => {
-    console.log("*** Begin Daily Update ***");
+// TODO: Don't post unplaced players
+function postUpdate(players, guilds) {
+    const leaderList = [...players].filter(player => player.stats.best > 0).sort((a,b) => b.stats.best - a.stats.best );
+    let leaders = "";
+    for (let i = 0; i < leaderList.length; i++) {
+        const p = leaderList[i];
+        leaders = leaders + `${i+1}. **${p.displayName}** (🛡${p.stats.tank.rank || "*[unplaced]*"}/⚔${p.stats.damage.rank || "*[unplaced]*"}/❤${p.stats.support.rank || "*[unplaced]*"})\n`;
+    }
 
+    if (leaders == "")
+        leaders = "Oops! Looks like noone has been added yet. Use '!mojito add <battletag>' to get started.";
+
+    const tankList = [...players].filter(player => player.stats.tank.rank > 0).sort((a,b) => b.stats.tank.rank - a.stats.tank.rank );
+    let tanks = "";
+    for (let i = 0; i < tankList.length; i++) {
+        const p = tankList[i];
+        tanks = tanks + `${i+1}. **${p.displayName}** (${p.stats.tank.rank || "*unplaced*"})\n`;
+    }
+
+    const dpsList = [...players].filter(player => player.stats.damage.rank > 0).sort((a,b) => b.stats.damage.rank - a.stats.damage.rank );
+    let dps = "";
+    for (let i = 0; i < dpsList.length; i++) {
+        const p = dpsList[i];
+        dps = dps + `${i+1}. **${p.displayName}** (${p.stats.damage.rank || "*unplaced*"})\n`;
+    }
+
+    const supportList = [...players].filter(player => player.stats.support.rank > 0).sort((a,b) => b.stats.support.rank - a.stats.support.rank );
+    let supports = "";
+    for (let i = 0; i < supportList.length; i++) {
+        const p = supportList[i];
+        supports = supports + `${i+1}. **${p.displayName}** (${p.stats.support.rank || "*unplaced*"})\n`;
+    }
+
+    const winnersList = [...players].filter(p => p.gains.best.gain > 0).sort((a,b) => b.gains.best.gain - a.gains.best.gain);
+    let winners = "";
+    for (let i = 0; i < winnersList.length; i++) {
+        const p = winnersList[i];
+        winners = winners + `${i+1}. **${p.displayName}** *(${getSigned(p.gains.best.gain)} ${p.gains.best.role})*\n`;
+    }
+
+    const losersList = [...players].filter(p => p.gains.worst.gain < 0).sort((a,b) => b.gains.worst.gain - a.gains.worst.gain);
+    let losers = "";
+    for (let i = 0; i < losersList.length; i++) {
+        const p = losersList[i];
+        losers = losers + `${i+1}. **${p.displayName}** *(${getSigned(p.gains.worst.gain)} ${p.gains.worst.role})*\n`;
+    }
+
+    guilds.forEach(guild => {
+        if (guild.available) {
+            console.log('Attempting to post update to server ' + guild.name);
+
+            let message = {
+                color: 5036231,
+                author: {
+                    name: "Cocktail Riot",
+                    icon_url: "https://cdn.discordapp.com/icons/420155827689619456/41e74064327e13e1c85421bbde50f065.webp"
+                },
+                fields: [
+                    {
+                        name: "Leaderboard",
+                        value: leaders || '\u200B'
+                    },
+                    {
+                        name: '\u200B',
+                        value: '\u200B',
+                    },
+                    {
+                        name: "🛡 Tank",
+                        value: tanks || '\u200B',
+                        inline: true
+                    },
+                    {
+                        name: "⚔ DPS",
+                        value: dps || '\u200B',
+                        inline: true
+                    },
+                    {
+                        name: "❤ Support",
+                        value: supports || '\u200B',
+                        inline: true
+                    },
+                    {
+                        name: '\u200B',
+                        value: '\u200B',
+                    },
+                    {
+                        name: "Biggest Winners",
+                        value: winners || '*No gains today :disappointed:*'
+                    },
+                    {
+                        name: "Biggest Losers",
+                        value: losers || '*No losses today :joy:*'
+                    }
+                ]
+            };
+
+            guild.channels.find(c => c.name === 'general')
+                .send('Good morning (former) Cocktail Riot! Here is your daily SR update...', {embed: message});
+        }
+    });
+}
+
+function updateDb(players) {
+    // Update database with new stats argh
+    console.log('Updating DB');
+    var docClient = new AWS.DynamoDB.DocumentClient();
+    players.forEach(player => {
+
+        var newParams = {
+            TableName: 'MojitoUsers',
+            Key: {
+                'Battletag': player.Battletag.S
+            },
+            UpdateExpression: "set TankSR = :tank, DamageSR = :dps, SupportSR = :support",
+            ExpressionAttributeValues: {
+                ":tank": player.stats.tank.rank || "0",
+                ":dps": player.stats.damage.rank || "0",
+                ":support": player.stats.support.rank || "0"
+            },
+            ReturnValues: "UPDATED_NEW"
+        };
+
+        docClient.update(newParams, (err, data) => {
+            if (err) {
+                console.error(`Unable to update ${player.Battletag.S}: ${JSON.stringify(err, null, 2)}`);
+            }
+        });
+    });
+}
+
+function dailyUpdate() {
     var db = new AWS.DynamoDB({apiVersion: '2012-08-10'});
     var params = {
         TableName: 'MojitoUsers'
@@ -120,174 +289,33 @@ schedule.scheduleJob({hour: 9, minute: 30}, () => {
     db.scan(params).promise()
     .then(results => results.Items)
     .then(users => {
-      Promise.all(
-        users.map(user => {
-          return getProfile('pc','eu', user.Battletag.S.replace('#', '-'))
-          .then(player => {
-            user.stats = {
-                tank: {
-                    rank: player.competitive.tank.rank,
-                    gain: player.competitive.tank.rank - user.TankSR.N
-                },
-                damage: {
-                    rank: player.competitive.damage.rank,
-                    gain: player.competitive.damage.rank - user.DamageSR.N
-                },
-                support: {
-                    rank: player.competitive.support.rank,
-                    gain: player.competitive.support.rank - user.SupportSR.N
-                },
-                total: player.competitive.support.rank + player.competitive.damage.rank + player.competitive.tank.rank
-            };
-            user.displayName = user.Battletag.S.split('#')[0];
-            user.gains = {
-                best: {
-                    gain: Math.max(user.stats.tank.gain, user.stats.damage.gain, user.stats.support.gain)
-                },
-                worst: {
-                    gain: Math.min(user.stats.tank.gain, user.stats.damage.gain, user.stats.support.gain)
-                }
-            };
-            user.gains.best.role = getGainRole(user.stats, user.gains.best.gain);
-            user.gains.worst.role = getGainRole(user.stats, user.gains.worst.gain);
-            return user;
-          })
-        }))
+      getUpdate(users)
         .then(players => {
-
-            const leaderList = [...players].sort((a,b) => a.stats.total - b.stats.total );
-            let leaders = "";
-            for (let i = 0; i < leaderList.length; i++) {
-                const p = leaderList[i];
-                leaders = leaders + `${i+1}. **${p.displayName}** (🛡${p.stats.tank.rank}/⚔${p.stats.damage.rank}/❤${p.stats.support.rank})`;
-            }
-
-            if (leaders == "")
-                leaders = "Oops! Looks like noone has been added yet. Use '!mojito add <battletag>' to get started.";
-
-            const tankList = [...players].sort((a,b) => a.stats.tank.rank - b.stats.tank.rank );
-            let tanks = "";
-            for (let i = 0; i < tankList.length; i++) {
-                const p = tankList[i];
-                const sr = p.stats.tank.rank > 0 ? p.stats.tank.rank.toString() : "*unplaced*";
-                tanks = tanks + `${i+1}. **${p.displayName}** (${sr})`;
-            }
-
-            const dpsList = [...players].sort((a,b) => a.stats.damage.rank - b.stats.damage.rank );
-            let dps = "";
-            for (let i = 0; i < dpsList.length; i++) {
-                const p = dpsList[i];
-                const sr = p.stats.damage.rank > 0 ? p.stats.damage.rank.toString() : "*unplaced*";
-                dps = dps + `${i+1}. **${p.displayName}** (${sr})`;
-            }
-
-            const supportList = [...players].sort((a,b) => a.stats.support.rank - b.stats.support.rank );
-            let supports = "";
-            for (let i = 0; i < supportList.length; i++) {
-                const p = supportList[i];
-                const sr = p.stats.support.rank > 0 ? p.stats.support.rank.toString() : "*unplaced*";
-                supports = supports + `${i+1}. **${p.displayName}** (${sr})`;
-            }
-
-            const winnersList = [...players].sort((a,b) => a.gains.best.gain - b.gains.best.gain);
-            let winners = "";
-            for (let i = 0; i < winnersList.length; i++) {
-                const p = winnersList[i];
-                if (p.gains.best.gain > 0)
-                    winners = winners + `${i+1}. **${p.displayName}** *(${getSigned(p.gains.best.gain)} ${p.gains.best.role})*`;
-            }
-
-            const losersList = [...players].sort((a,b) => a.gains.worst.gain - b.gains.worst.gain);
-            let losers = "";
-            for (let i = 0; i < losersList.length; i++) {
-                const p = losersList[i];
-                if (p.gains.worst.gain < 0)
-                    losers = losers + `${i+1}. **${p.displayName}** *(${getSigned(p.gains.worst.gain)} ${p.gains.worst.role})*`;
-            }
-    
-            bot.guilds.forEach(guild => {
-                if (guild.available) {
-                    console.log('Attempting to post update to server ' + guild.name);
-
-                    let message = {
-                        color: 5036231,
-                        author: {
-                            name: "Cocktail Riot",
-                            icon_url: "https://cdn.discordapp.com/icons/420155827689619456/41e74064327e13e1c85421bbde50f065.webp"
-                        },
-                        fields: [
-                            {
-                                name: "Leaderboard",
-                                value: leaders || '\u200B'
-                            },
-                            {
-                                name: '\u200B',
-                                value: '\u200B',
-                            },
-                            {
-                                name: "🛡 Tank",
-                                value: tanks || '\u200B',
-                                inline: true
-                            },
-                            {
-                                name: "⚔ DPS",
-                                value: dps || '\u200B',
-                                inline: true
-                            },
-                            {
-                                name: "❤ Support",
-                                value: supports || '\u200B',
-                                inline: true
-                            },
-                            {
-                                name: '\u200B',
-                                value: '\u200B',
-                            },
-                            {
-                                name: "Biggest Winners",
-                                value: winners || '*No gains today :disappointed:*'
-                            },
-                            {
-                                name: "Biggest Losers",
-                                value: losers || '*No losses today :joy:*'
-                            }
-                        ]
-                    };
-        
-                    guild.channels.find(c => c.name === 'general')
-                        .send('Good morning (former) Cocktail Riot! Here is your daily SR update...', {embed: message});
-                }
-            });
-
-            // Update database with new stats argh
-            console.log('Updating DB');
-            var docClient = new AWS.DynamoDB.DocumentClient();
-            players.forEach(player => {
-
-                var newParams = {
-                    TableName: 'MojitoUsers',
-                    Key: {
-                        'Battletag': player.Battletag.S
-                    },
-                    UpdateExpression: "set TankSR = :tank, DamageSR = :dps, SupportSR = :support",
-                    ExpressionAttributeValues: {
-                        ":tank": player.stats.tank.rank,
-                        ":dps": player.stats.damage.rank,
-                        ":support": player.stats.support.rank
-                    },
-                    ReturnValues: "UPDATED_NEW"
-                };
-
-                docClient.update(newParams, (err, data) => {
-                    if (err) {
-                        console.error(`Unable to update ${player.Battletag.S}: ${JSON.stringify(err, null, 2)}`);
-                    }
-                });
-            });
-
+            postUpdate(players, bot.guilds);
+            updateDb(players);
         });
       });
+}
+
+function adhocUpdate(guilds) {
+    var db = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+    var params = {
+        TableName: 'MojitoUsers'
+    };
     
-}); // end schedule
+    db.scan(params).promise()
+    .then(results => results.Items)
+    .then(users => {
+      getUpdate(users)
+        .then(players => {
+            postUpdate(players, guilds);
+        });
+      });
+}
+
+schedule.scheduleJob({hour: 9, minute: 0}, () => {
+    console.log("*** Begin Daily Update ***");
+    dailyUpdate();
+});
 
 bot.login(process.env.MOJ_TOKEN);
